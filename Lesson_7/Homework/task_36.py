eab483d3596b6c0b06c9f70f676b6f24d8c6be38
Aleksey Kuritsyn <SciWake@gmail.com>
2023-04-07 02:47:49 +0700

    Разбор домашнего задания 7

---
+++ Lesson_7/Homework/task_36.py
@@ -0,0 +1,22 @@
+"""Задача 36: Напишите функцию 
+print_operation_table(operation, num_rows=6, num_columns=6), 
+которая принимает в качестве аргумента функцию, вычисляющую 
+элемент по номеру строки и столбца. Аргументы num_rows и 
+num_columns указывают число строк и столбцов таблицы, 
+которые должны быть распечатаны. Нумерация строк и столбцов 
+идет с единицы (подумайте, почему не с нуля). Примечание: 
+бинарной операцией называется любая операция, у которой 
+ровно два аргумента, как, например, у операции умножения."""
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    if num_rows < 2 or num_columns < 2:
+        print('ОШИБКА! Размерности таблицы должны быть больше 2!')
+    else:
+        for i in range(1, num_rows + 1):
+            for j in range(1, num_columns + 1):
+                print(operation(i, j), end='	')
+            print()
+
+
+print_operation_table(lambda x, y: x * y + 1)
